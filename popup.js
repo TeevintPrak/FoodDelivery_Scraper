1fb053e18d76a92bb00386f7c0e981254a8386e5
TeevintPrak <teevint19@gmail.com>
2024-04-01 04:28:08 -0700

    Added stub scraping function

--- popup.js
+++ popup.js
@@ -1,5 +1,17 @@
 let scrapeApp = document.getElementById('scrapeApp');
 
-scrapeApp.addEventListener("click", () => {
-    alert('hello');
+// Function to scrape data from food delivery web app
+function scrapeDataFromPage() {
+    alert('scraping');
+}
+
+scrapeApp.addEventListener("click", async () => {
+    //Get current active tab
+    let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+
+    //Execute script to scrape the data from the food web app.
+    chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+        func: scrapeDataFromPage,
+    });
 })
