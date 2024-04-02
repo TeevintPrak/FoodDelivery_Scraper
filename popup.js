c5fd0d98770ff31e71672a5096dde0e95e57ed2c
TeevintPrak <teevint19@gmail.com>
2024-04-01 22:23:25 -0700

    Complete summary statistics and display on chrome extension

--- popup.js
+++ popup.js
@@ -1,7 +1,33 @@
 let scrapeApp = document.getElementById('scrapeApp');
 
 function scrapeDataFromPage() {
+    function parseData(restaurantName, ratingText, deliveryTimeText) {
+
+        const rating = parseFloat(ratingText);    
+        const earliest = parseInt(deliveryTimeText.substring(0,2));
+        const latest = parseInt(deliveryTimeText.substring(3,5));
+    
+        const arr = [
+            restaurantName,
+            rating,
+            earliest,
+            latest
+        ];
+        return arr;
+    }
+
+    function findAverageRating(arr) {
+        const sum = arr.reduce((partialSum, curr) => partialSum + curr, 0);
+        const average = Math.round((sum / arr.length)*100)/100;
+        return average;
+    }
+
     const results = [];
+    const restaurantNames = [];
+    const ratings = [];
+    const earliestDeliveryTime = [];
+    const latestDeliveryTime = [];
+    const medianDeliveryTime = [];
     // Use the current page's URL
     const url = window.location.href;
     console.log('Current URL:', url);
@@ -15,7 +41,7 @@ function scrapeDataFromPage() {
 
     console.log("# of cards" + cards.length);
 
-    cards.forEach(card => {
+    cards.forEach((card, index) => {
         // Extract the restaurant name directly
         const name = card.children[0].querySelector('h3')?.innerText.trim();
 
@@ -31,29 +57,78 @@ function scrapeDataFromPage() {
         const deliveryTimeMatch = infoText.match(deliveryTimeRegex);
 
         // Extract the first match (if any) as the desired value
-        const ratingText = ratingMatch ? ratingMatch[0] : 'No rating info';
-        const deliveryTimeText = deliveryTimeMatch ? deliveryTimeMatch[0] : 'No delivery time info';
+        const ratingText = ratingMatch ? ratingMatch[0] : null;
+        const deliveryTimeText = deliveryTimeMatch ? deliveryTimeMatch[0] : null;
 
-        results.push({
+        // Remove if there is a null value
+        if(name && ratingText && deliveryTimeText){
+            //parse the data into float, ints, and seperating numbers. 
+            var arr = parseData(name, ratingText, deliveryTimeText);
+            results.push(arr);
+            restaurantNames.push(arr[0]);
+            ratings.push(arr[1]);
+            earliestDeliveryTime.push(arr[2]);
+            latestDeliveryTime.push(arr[3]);
+            medianDeliveryTime.push((arr[2] + arr[3])/2);
+        }
+        
+        /*
+        results.push(
             name,
             rating: ratingText,
             deliveryTime: deliveryTimeText
-        });
+        });*/
+
     });
 
-    console.log("Displaying results...");
-    console.log(results);
+    const averageRating = findAverageRating(ratings);
+    const highestRatedIndex = ratings.reduce((iMax, x, i, ratings) => x > ratings[iMax] ? i : iMax, 0);
+    const lowestRatedIndex = ratings.reduce((iLow, x, i, ratings) => x < ratings[iLow] ? i : iLow, 0);
+    const longestDeliveryIndex = medianDeliveryTime.reduce((iMax, x, i, ratings) => x > ratings[iMax] ? i : iMax, 0);
+    const shortestDeliveryIndex = medianDeliveryTime.reduce((iLow, x, i, ratings) => x < ratings[iLow] ? i : iLow, 0);
 
-    return results;
+    const finalResults = ["Average Rating: " + averageRating, 
+    "Highest Rated Restaurant: " + restaurantNames[highestRatedIndex], 
+    "Highest  Rating: " + ratings[highestRatedIndex], 
+    "Lowest Rated Restaurant: " + restaurantNames[lowestRatedIndex], 
+    "Lowest Rating: " + ratings[lowestRatedIndex], 
+    "Longest Delivery Restaurant: " + restaurantNames[longestDeliveryIndex], 
+    "Longest Delivery Time: " + medianDeliveryTime[longestDeliveryIndex] + " min", 
+    "Shortest Delivery Restaurant: " + restaurantNames[shortestDeliveryIndex], 
+    "Shortest Delivery Time: " + medianDeliveryTime[shortestDeliveryIndex] + " min"];
+
+    console.log("Average Rating:" + finalResults[0]);
+    console.log("Highest Rated Restaurant:" + finalResults[1] + " with  Rating: " + finalResults[2]);
+    console.log("Lowest Rated Restaurant:" + finalResults[3] + " with Rating: " + finalResults[4]);
+    console.log("Longest Delivery Restaurant:" + finalResults[5] + " with Time: " + finalResults[6] + " mins");
+    console.log("Shortest Delivery Restaurant:" + finalResults[7] + " with Time: " + finalResults[8] + " mins");
+
+    return finalResults;
+}
+
+function generateList(arg) {
+    let items = "";
+    for(let i = 0; i < arg.length; i++) {
+        items += `<li>${arg[i]}</li>`;
+    }
+    return items;
 }
 
 scrapeApp.addEventListener("click", async () => {
     //Get current active tab
     let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
 
-    //Execute script to scrape the data from the food web app.
+    /*
     chrome.scripting.executeScript({
         target: {tabId: tab.id},
         function: scrapeDataFromPage,
+    });*/
+
+    const executeScript = (tabId, func) => new Promise(resolve => {
+        chrome.scripting.executeScript({ target: {tabId}, func}, resolve)
     });
+
+    const [{result: finalResults}] = await executeScript(tab.id, scrapeDataFromPage);
+    
+    document.querySelector("main").innerHTML = `<ul>${generateList(finalResults)}</ul>`;
 });
